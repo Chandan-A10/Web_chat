e92264b30cd152cebc00e473c523a355e7376e7a
Chandan <chandan1226.be20@chitkarauniversity.edu.in>
2023-06-28 09:26:23 +0530

    Final

--- src/Database/CreateChatroom.js
+++ src/Database/CreateChatroom.js
@@ -1,10 +1,16 @@
-import { addDoc } from "firebase/firestore"
+import { addDoc, getDocs, query, where } from "firebase/firestore"
 import { ChatRoomCollection } from "../firebase/firebase"
 
 export const CreateChatRoom=async(chatid)=>{
+    const qry=query(ChatRoomCollection,where('chatID','==',chatid))
+    const chatroom=await getDocs(qry)
+    if(!chatroom.empty){
+        return
+    }
     const newroom= await addDoc(ChatRoomCollection,{
         chatID:chatid,
         chats:[]
     })
     console.log('Created Room')
+    return
 }
